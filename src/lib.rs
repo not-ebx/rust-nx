e3d52fdbc17a9404e8189d531f9f79c1403e8053
not-ebx <28018026+not-ebx@users.noreply.github.com>
2024-04-27 21:59:42 -0400

    Working properly so far. Tested with strings, numbers, vectors. bitmap and audio still not planned, honestly.

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,27 @@ mod nx_file;
 #[cfg(test)]
 mod tests {
     use crate::nx_file::NXFile;
-    use super::*;
 
     #[test]
     fn it_works() {
-        let res = NXFile::new("/Users/knux/Documents/Programming/self/OSS-Projects/rustNX/String.nx");
+        let res = NXFile::new("/Users/knux/Documents/Programming/self/OSS-Projects/rustNX/test/String.nx");
         assert_eq!(res.is_ok(), true);
-
-        let mut nx_file = res.unwrap();
-        let parser = nx_file.parse();
-
-        assert_eq!(parser.is_ok(), true);
+        let string_nx = res.unwrap();
+        let cash_img = string_nx.resolve("Cash.img").unwrap();
+        let cash_items = string_nx.get_node_children(cash_img).unwrap();
+        for item in cash_items.iter() {
+            let item_nodes = string_nx.get_node_children(item).unwrap();
+            for node in item_nodes.iter() {
+                match node.name.as_str() {
+                    "name" => {
+                        print!("Name: {:?}\n", node.data)
+                    },
+                    "desc" => {
+                        print!("Desc: {:?}\n", node.data)
+                    },
+                    _ => {}
+                }
+            }
+        }
     }
 }
--- src/nx_file.rs
+++ src/nx_file.rs
@@ -1,3 +1,4 @@
+use std::cmp::PartialEq;
 use std::fs::File;
 use std::io::{BufReader, Error, Read, Seek, SeekFrom};
 use std::path::PathBuf;
@@ -6,6 +7,7 @@ use crate::nx_file::NXNodeType::{AUDIO, BITMAP, DOUBLE, INT64, NONE, STRING, VEC
 
 const MAGIC_BYTES: &str = "PKG4";
 
+#[derive(Clone, Copy)]
 enum NXNodeType {
     NONE,
     INT64, // 64 bit signed int
@@ -14,6 +16,21 @@ enum NXNodeType {
     VECTOR, //
     BITMAP, //
     AUDIO //
+}
+
+#[derive(Debug)]
+pub enum NXNodeData {
+    String(String),
+    Bitmap(NXBitmapData),
+    Audio(NXAudioData),
+    Int64(i64),
+    Double(f64),
+    Vector(NXVectorData),
+    None
+}
+
+impl NXNodeData {
+
 }
 
 impl From<u16> for NXNodeType {
@@ -31,72 +48,74 @@ impl From<u16> for NXNodeType {
     }
 }
 
-struct NXVectorData {
+#[derive(Debug)]
+pub struct NXVectorData {
     x: i32,
     y: i32
 }
 
-struct NXBitmapData {
+impl NXVectorData {
+    pub fn new(data: &[u8; 8]) -> Self {
+        let data_x = &data[0..4];
+        let data_y = &data[4..];
+
+        NXVectorData {
+            x: LittleEndian::read_i32(data_x),
+            y: LittleEndian::read_i32(data_y)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct NXBitmapData {
     id: u32,
     width: u16,
     height: u16
 }
 
-struct NXAudioData {
+
+impl NXBitmapData {
+    pub fn new(data: &[u8; 8]) -> Self {
+        NXBitmapData {
+            id: LittleEndian::read_u32(&data[0..4]),
+            width: LittleEndian::read_u16(&data[4..6]),
+            height: LittleEndian::read_u16(&data[6..]),
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct NXAudioData {
     id: u32,
     length: u32
 }
 
-struct NXStringData {
-    id: u32,
-    as_string: str
-}
-
-struct NXBitmapImg {
-    length: u32,
-    data: Vec<u8>
-}
-
-pub struct NXNode<'a> {
-    file_data: &'a NXFileData,
-    name_id: u32, // String ID
-    name: String,
-    child: u32, // Node ID of first child
-    n_child: u16, // amount of child
-    ntype: NXNodeType,
-    data: [u8; 8],
-}
-
-impl NXNode<'_> {
-    fn get_string(&self) -> &str {
-        // TODO find on table
-        let string_id = LittleEndian::read_u32(&self.data);
-
-
-        return "";
-    }
-    fn get_bitmap(&self) -> NXBitmapData {
-        NXBitmapData {
-            id: LittleEndian::read_u32(&self.data[0..4]),
-            width: LittleEndian::read_u16(&self.data[4..6]),
-            height: LittleEndian::read_u16(&self.data[6..]),
-        }
-    }
-    fn get_audio(&self) -> NXAudioData {
+impl NXAudioData {
+    pub fn new(data: &[u8; 8]) -> Self {
         NXAudioData {
-            id: LittleEndian::read_u32(&self.data[0..4]),
-            length: LittleEndian::read_u32(&self.data[4..]),
+            id: LittleEndian::read_u32(&data[0..4]),
+            length: LittleEndian::read_u32(&data[4..]),
         }
     }
-
-    fn get_vector(&self) -> NXVectorData {
-        NXVectorData {
-            x: LittleEndian::read_i32(&self.data[0..4]),
-            y: LittleEndian::read_i32(&self.data[4..]),
-        }
-    }
-
 }
+
+pub struct NXNode {
+    name_id: u32, // String ID
+    pub name: String,
+    pub child: u32, // Node ID of first child
+    pub n_child: u16, // amount of child
+    ntype: NXNodeType,
+    pub data: NXNodeData,
+}
+
+
+impl NXNode {
+    pub fn has_children(&self) -> bool {
+        self.n_child > 0
+    }
+}
+
 
 pub struct NXFileHeader {
     magic : u32,
@@ -117,14 +136,14 @@ pub struct NXFileData {
     bitmaps: Vec<NXBitmapData>,
 }
 
-pub struct NXFile<'a> {
+pub struct NXFile {
     file_path: PathBuf,
     freader: BufReader<File>,
     file_data: NXFileData,
-    nodes : Vec<NXNode<'a>>,
+    nodes : Vec<NXNode>,
 }
 
-impl<'a> NXFile<'a> {
+impl NXFile {
     pub fn new(file_path: &str) -> Result<Self, Error> {
         let file_path_buf = PathBuf::from(file_path);
         let file = File::open(file_path_buf)?;
@@ -166,20 +185,60 @@ impl<'a> NXFile<'a> {
             audio_offset,
         };
 
+        let nx_file_data = NXFileData{
+            header,
+            strings: vec![],
+            audios: vec![],
+            bitmaps: vec![],
+        };
 
+        let mut nx_file = NXFile{
+            file_path: PathBuf::from(file_path),
+            freader: file_reader,
+            file_data: nx_file_data,
+            nodes: vec![],
+        };
+
+        nx_file.load_tables()?;
+        nx_file.load_nodes()?;
+
+        Ok(nx_file)
+    }
+
+    fn create_node_data(&self, ntype: NXNodeType, data: [u8; 8]) -> NXNodeData {
+        match ntype {
+            INT64 => {NXNodeData::Int64(LittleEndian::read_i64(&data))},
+            DOUBLE => {NXNodeData::Double(LittleEndian::read_f64(&data))},
+            STRING => {NXNodeData::String(self.get_string(LittleEndian::read_u32(&data)))},
+            VECTOR => {NXNodeData::Vector(NXVectorData::new(&data))},
+            BITMAP => {NXNodeData::Bitmap(NXBitmapData::new(&data))},
+            AUDIO => {NXNodeData::Audio(NXAudioData::new(&data))},
+            _ => { NXNodeData::None}
+        }
+    }
+
+    fn get_string(&self, string_id: u32) -> String {
+        let string_text = self.file_data.strings.get(string_id as usize);
+        match string_text {
+            Some(text) => {text.to_string()},
+            None => { "".to_string() }
+        }
+    }
+
+    fn load_tables(&mut self) -> Result<(), Error>{
         // CREATE THE TABLES
         // String table
-        file_reader.seek(SeekFrom::Start(string_offset))?;
-        let mut string_table : Vec<String> = vec!["".to_string(); string_count as usize];//Vec::with_capacity(string_count as usize);
+        self.freader.seek(SeekFrom::Start(self.file_data.header.string_offset))?;
+        let mut string_table : Vec<String> = vec!["".to_string(); self.file_data.header.string_count as usize];
         for curr_str in string_table.iter_mut() {
-            let offset = file_reader.read_u64::<LittleEndian>()?;
+            let offset = self.freader.read_u64::<LittleEndian>()?;
             // MARK HERE
-            let current_offset = file_reader.seek(SeekFrom::Current(0))?;
+            let current_offset = self.freader.seek(SeekFrom::Current(0))?;
             //
-            file_reader.seek(SeekFrom::Start(offset))?;
-            let string_length = file_reader.read_u16::<LittleEndian>()?;
+            self.freader.seek(SeekFrom::Start(offset))?;
+            let string_length = self.freader.read_u16::<LittleEndian>()?;
             let mut string_data: Vec<u8> = vec![0; string_length as usize];
-            file_reader.read_exact(&mut string_data)?;
+            self.freader.read_exact(&mut string_data)?;
 
             let string_utf8 = String::from_utf8(string_data).map_err(
                 |e| std::io::Error::new(std::io::ErrorKind::InvalidData, e)
@@ -188,39 +247,44 @@ impl<'a> NXFile<'a> {
             *curr_str = string_utf8.to_string();
 
             // Reset to the beginning of the offset
-            file_reader.seek(SeekFrom::Start(current_offset))?;
+            self.freader.seek(SeekFrom::Start(current_offset))?;
         }
+        self.file_data.strings = string_table;
 
         // TODO:
         // Bitmap & Audio.
         // I dont need them atm, but ill probably add it... later... maybe
-        let nx_file_data = NXFileData{
-            header,
-            strings: string_table,
-            audios: vec![],
-            bitmaps: vec![],
-        };
+        // Bitmap Table
+        /*
+        self.freader.seek(SeekFrom::Start(bitmap_offset))?;
+        let mut bitmap_table : Vec<NXBitmapImg> = Vec::with_capacity(bitmap_count as usize);
+        for i in 0..bitmap_count {
+            let offset = self.freader.read_u64::<LittleEndian>()?;
+            let current_offset = self.freader.seek(SeekFrom::Current(0))?;
+            self.freader.seek(SeekFrom::Start(offset))?;
 
+            let bitmap_length = self.freader.read_u32::<LittleEndian>()?;
+            let mut bitmap_data : Vec<u8> = vec![0; bitmap_length as usize];
+            self.freader.read_exact(&mut bitmap_data)?;
 
-        Ok(NXFile{
-            file_path: PathBuf::from(file_path),
-            freader: file_reader,
-            file_data: nx_file_data,
-            nodes: vec![],
-        })
+            bitmap_table.push(NXBitmapImg{
+                length: bitmap_length,
+                data: bitmap_data,
+            });
+            self.freader.seek(SeekFrom::Start(current_offset))?;
+        }
+        self.file_data.bitmaps = bitmap_table;
+        */
+        Ok(())
     }
 
     // Create the nodes
-    pub fn parse(&mut self) -> Result<Vec<NXNode>, Error>{
+    fn load_nodes(&mut self) -> Result<(), Error>{
         // Create the node array I guess xd
         self.freader.seek(SeekFrom::Start(self.file_data.header.node_offset))?;
         let mut nodes : Vec<NXNode> = Vec::with_capacity(self.file_data.header.node_count as usize);
-        for i in 0..self.file_data.header.node_count {
+        for _  in 0..self.file_data.header.node_count {
             // Name
-            //let str_length = file_reader.read_u16::<LittleEndian>()?;
-            //let mut node_name_bytes: Vec<u8> = Vec::with_capacity(str_length as usize);
-            //file_reader.read_exact(&mut node_name_bytes)?;
-            //let node_name = std::str::from_utf8(&node_name_bytes)?;
             let node_name_id = self.freader.read_u32::<LittleEndian>()?;
             let node_name = self.file_data.strings.get(node_name_id as usize).unwrap_or(&String::new()).clone();
 
@@ -230,18 +294,54 @@ impl<'a> NXFile<'a> {
             let ntype: NXNodeType = self.freader.read_u16::<LittleEndian>()?.into();
 
             // Create depending on the type.
-            let node_data = self.freader.read_u64::<LittleEndian>()?.to_le_bytes();
-             nodes.push(NXNode{
-                file_data: &self.file_data,
+            let node_data: [u8; 8] = self.freader.read_u64::<LittleEndian>()?.to_le_bytes();
+
+            nodes.push(NXNode{
                 name_id: node_name_id,
                 name: node_name,
                 child,
                 n_child,
                 ntype,
-                data: node_data,
+                data: self.create_node_data(ntype, node_data),
             });
         }
 
-        Ok(nodes)
+        self.nodes = nodes;
+        Ok(())
+    }
+
+    pub fn get_node_children(&self, node: &NXNode) -> Option<&[NXNode]> {
+        let start_i = node.child as usize;
+        let end_i = start_i + node.n_child as usize;
+
+        if end_i <= self.nodes.len() {
+            return Some(&self.nodes[start_i..end_i])
+        }
+        None
+    }
+
+    pub fn resolve(&self, full_path: &str) -> Option<&NXNode> {
+        let node_path: Vec<&str> = full_path.split("/").collect();
+        if node_path.len() <= 0 {
+            return None
+        }
+
+        let mut current_node : &NXNode = &self.nodes[0];
+        // Search for the first one
+        for (_, path) in node_path.iter().enumerate() {
+            let node_cursor = self.get_node_children(current_node)?;
+            let node_result = node_cursor.iter().find(
+                |&x| x.name.eq(path)
+            );
+            match node_result {
+                Some(x) => {
+                    current_node = x
+                },
+                None => {
+                    return None
+                }
+            }
+        }
+        Some(current_node)
     }
 }
